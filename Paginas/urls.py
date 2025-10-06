4ee5366a79b681d3184418ef7d8fd0b7439d3865
Nico Haugaard <nicohaugaard@gmail.com>
2025-10-06 13:05:57 -0300

    Agregue listado y detalle de novedades

--- Paginas/urls.py
+++ Paginas/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from .views import AboutView
+from .views import AboutView, PaginaListView, PaginaDetailView
 
 app_name = "paginas"
 
 urlpatterns = [
     path("about/", AboutView.as_view(), name="about"),
+    path("pages/", PaginaListView.as_view(), name="lista"),
+    path("pages/<slug:slug>/", PaginaDetailView.as_view(), name="detalle"),
 ]
--- Paginas/views.py
+++ Paginas/views.py
@@ -1,5 +1,21 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView, DetailView
 
+from .models import Pagina
 
 class AboutView(TemplateView):
     template_name = "paginas/about.html"
+
+
+class PaginaListView(ListView):
+    model = Pagina
+    template_name = "paginas/pagina_list.html"
+    context_object_name = "paginas"
+    paginate_by = 6
+
+
+class PaginaDetailView(DetailView):
+    model = Pagina
+    template_name = "paginas/pagina_detail.html"
+    context_object_name = "pagina"
+    slug_field = "slug"
+    slug_url_kwarg = "slug"
